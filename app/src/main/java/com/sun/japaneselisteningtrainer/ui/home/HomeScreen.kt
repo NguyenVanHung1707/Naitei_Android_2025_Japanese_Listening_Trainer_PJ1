fd5d63967e9d155c16c495ac9e5c478576b36426
NguyenVanHung1707 <hungnguyen.170704@gmail.com>
2025-08-18 18:02:39 +0700

    UI

--- app/src/main/java/com/sun/japaneselisteningtrainer/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sun/japaneselisteningtrainer/ui/home/HomeScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
@@ -21,7 +20,6 @@ import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -71,18 +69,6 @@ fun HomeScreen(
                     containerColor = MaterialTheme.colorScheme.primary
                 )
             )
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = navigateToAudioEntry,
-                containerColor = MaterialTheme.colorScheme.tertiary
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = "Add",
-                    tint = MaterialTheme.colorScheme.onTertiary
-                )
-            }
         },
         containerColor = MaterialTheme.colorScheme.background,
         bottomBar = {
